026f7fac779810d2e2278509a258223a037ed6de
Malik Rasaq <malikrasaq@gmail.com>
2023-09-09 08:32:13 +0100

    Added design assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:dribbox/src/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'src/screens/screens.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
