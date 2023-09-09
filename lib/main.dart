48ed27d7ae3dffb926479e078f80cb9972bae0c8
Malik Rasaq <malikrasaq@gmail.com>
2023-09-09 08:05:01 +0100

    Setup folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
+import 'package:dribbox/src/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: const MaterialApp(
+        title: 'Dribbox',
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
       ),
     );
   }
